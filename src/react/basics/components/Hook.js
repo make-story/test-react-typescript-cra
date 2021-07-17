fc8ad95850c17b9249e7607e5d17e071dd4706b3
Sung-min Yu <yu9221@gmail.com>
2021-07-17 20:18:11 +0900

    update

--- src/react/basics/components/Hook.js
+++ src/react/basics/components/Hook.js
@@ -115,7 +115,9 @@ useEffect(didUpdate, [dependency]);
 
 // 컴포넌트
 const Hook = () => {   
-	// useState
+	/**
+	 * useState
+	 */
 	// 가변적인 상태를 지닐 수 있게 해줍니다.
 	// useState 함수의 파라미터에는 상태의 '기본값'을 넣어 줍니다.
 	// 이 함수가 호출되면 배열을 반환하는 데요. 그 배열의 첫 번째 원소는 '상태값', 두 번째 원소는 상태를 설정하는 '함수(세터함수)'입니다.
@@ -124,7 +126,10 @@ const Hook = () => {
 	const [list, setList] = useState([]);
 	const [number, setNumber] = useState('');
 	
-	// useEffect
+
+	/**
+	 * useEffect
+	 */
 	// useEffect 는 리액트 '컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정'할 수 있는 Hook
 	useEffect(() => {
 		console.log('--- --- useEffect');
@@ -133,6 +138,7 @@ const Hook = () => {
 		console.log('number', number);
 		console.log('--- ---');
 	});
+
 	// 마운트 될 때만 실행하고 싶을 때
 	// useEffect 에서 설정한 함수를 컴포넌트가 화면에 맨 처음 렌더링될 때만 실행하고, 
 	// '업데이트될 때는 실행하지 않으려면 함수의 두번째 파라미터로 비어 있는 배열'을 넣어 주면 된다.
@@ -141,6 +147,7 @@ const Hook = () => {
 		console.log('마운트될 때만 실행됩니다.');
 		console.log('--- ---');
 	}, []);
+
 	// 특정 값이 업데이트될 때만 실행하고 싶을 때 - useState 를 통해 특정 상태 값 업데이트가 발생했을 때, 실행될 수 있도록 할 수 있다. (useState 상태값 업데이트 모니터링)
 	// useEffect 를 사용할 때, 특정 값이 변경될 때만 호출하고 싶을 경우도 있겠지요?
 	// useEffect 의 두 번째 파리미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어주면, '해당 값이 바뀔때만 실행'
@@ -149,6 +156,7 @@ const Hook = () => {
 		console.log('number 값 변경 발생', number);
 		console.log('--- ---');
 	}, [number]);
+
 	// 뒷정리하기
 	// useEffect 는 '기본적으로 렌더링되고 난 직후마다 실행'되며, 두 번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라집니다.
 	// 컴포넌트가 '언마운트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면, useEffect 에서 뒷정리(cleanup) 함수를 반환'해 주어야 합니다.
@@ -163,6 +171,12 @@ const Hook = () => {
 			console.log('--- ---');
 		};
 	});
+
+	// 의존 관계 배열 (배열 내 값의 변경이 있을 때만 useEffect 실행)
+	useEffect(() => {
+		//
+	}, [list, number]); // list 또는 number 값의 변경이 있을 때 실행
+
 	// async 함수는 promise 객체를 리턴하기 때문에
 	// useEffect 함수 자체를 async 함수로 사용할 수는 없다
 	/*useEffect(async () => { 
@@ -182,13 +196,17 @@ const Hook = () => {
 	}, []);*/
 
 
-	// useLayoutEffect
+	/**
+	 * useLayoutEffect
+	 */
 	// useEffect와 동일하긴 한데, 모든 DOM 변경 후에 동기적으로 발생
 	// 이것은 DOM에서 레이아웃을 읽고 동기적으로 리렌더링하는 경우에 사용
 	// useLayoutEffect의 내부에 예정된 갱신은 브라우저가 화면을 그리기 이전 시점에 동기적으로 수행
 
 
-	// useReducer
+	/**
+	 * useReducer
+	 */
 	// useReducer 는 useState 보다 더 다양한 컴포넌트 상황에 따라 상태를 다른 값으로 업데이트해 주고 싶을 때 사용하는 Hook 입니다.
 	// 리듀서는 현재 상태, 그리고 업데이트를 위해 필요한 정보를 담은 액션(action) 값을 전달받아 새로운 상태를 반환하는 함수입니다.
 	// 리듀서 함수에서 새로운 상태를 만들 때는 반드시 '불변성'을 지켜주어야 합니다.
@@ -203,18 +221,25 @@ const Hook = () => {
 	});
 	const { name, nickname } = stateCustom;
 
-	// useMemo
+
+	/**
+	 * useMemo
+	 */
 	// useMeno 를 사용하면 함수형 컴포넌트 내부에서 발생하는 연산을 최적화 할 수 있습니다.
 	// useMemo 는 렌더링하는 과정에서 특정 값이 바뀌었을 때만 연산을 실행하고, '원하는 값이 바뀌지 않았다면 이전에 연산 했던 결과를 다시 사용하는 방식'입니다.
 	const avg = useMemo(() => getAverage(list), [list]);
 
 
-	// useRef
+	/**
+	 * useRef
+	 */
 	// useRef 를 사용하여 ref 를 설정하면, useRef 를 통해 만든 객체 안의 current 값이 실제 엘리먼트룰 가리킵니다.
 	const inputElement = useRef(null);
 
 
-	// useCallback
+	/**
+	 * useCallback
+	 */
 	// useCallback 은 useMemo와 상당히 비슷한 함수입니다. 
 	// 주로 렌더링 성능을 최적화해야 하는 상황에서 사용하는데요.
 	// 이 Hook 을 사용하면 '이벤트 핸들러 함수를 필요할 때만 생성'할 수 있습니다. (컴포넌트가 리렌더링될 때마다 이벤트 핸들러 함수가 새로 생성되는 것 방지)
@@ -244,6 +269,9 @@ const Hook = () => {
 	);
 
 
+	/**
+	 * 커스텀 훅
+	 */
 	// 비동기 데이터 호출/로딩상태/에러상태 커스텀 Hook
 	const [articles, isError, isLoading] = useArticleApi();
 
